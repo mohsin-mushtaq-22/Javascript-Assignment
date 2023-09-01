a340814b3996892e697dce6e7336ad7eca08bf23
Mohsin Mushtaq <139048772+mohsin-mushtaq-22@users.noreply.github.com>
2023-08-31 23:57:04 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+// Alert Assignment
+
+// Question No 1
+// alert ("Welcome to my website")
+
+
+
+// Question no 2
+// alert("Error! Please enter a valid password.")
+
+
+
+//Question No 3
+// alert("Welcome to JS Land... \nHappy Coding!")
+
+
+// Question No 4
+// var javascrpitAlert = "Welcome to JS Land..."
+// var message = "Happy Coding! \nprevent this page from creating additional dialogs."
+// alert(javascrpitAlert)
+// alert(message)
+
+// Question No 5 Not able to understand 
+
+// Alert Assignment End Here
+
+
+
+
+
+// Variable for string Assignment
+
+// Question No 1 and 2
+// var userName = "Mohsin Mushtaq"
+// alert(userName)
+
+
+// Question No 3
+// var message= "Hello World";
+// alert(message)
+
+
+// Question No 4
+// var myName = "Mohsin Mushtaq."
+// var age = "26 year old"
+// var education = "Certified web and app developer"
+// alert(myName)
+// alert(age)
+// alert(education)
+
+
+// // Question No 5
+// var food = ("Pizza \nPizz \nPiz \nPi \nP")
+// alert(food)
+
+// Question No 6
+// var email = ("mohsinmushtaq40@gmail.com")
+// alert(email)
+
+// Question No 7
+// var Book = ("A smarter way to learn JavaScript")
+// alert(Book)
+
+
+// Question No 8
+// document.write('Yah! I can write HTML content through JavaScript')
+
+
+
+// // Question No 9
+// var design = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+// alert(design)
+// document.write(design)
+
+
+// Variable for String Assignment End
+
+
+
+
+
+
+
+                            
+
+
+
